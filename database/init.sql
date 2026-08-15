26d59923decf861824b90a32174704199eae1a0a
maurelo860@gmail.com <maurelo860@gmail.com>
2026-08-14 20:42:35 -0600

    feat-script: Se agrego el script de la DB como init.sql

---
+++ database/init.sql
@@ -0,0 +1,133 @@
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    tipo VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(150) NOT NULL,
+    telefono VARCHAR(20),
+    direccion VARCHAR(255),
+    dpi CHAR(13) NOT NULL UNIQUE,
+    correo VARCHAR(150) NOT NULL UNIQUE,
+    contrasena VARCHAR(255) NOT NULL,
+    
+    id_rol INT NOT NULL,
+    CONSTRAINT fk_usuario_rol FOREIGN KEY (id_rol) REFERENCES roles(id) ON DELETE RESTRICT
+);
+
+-- Añadí Cabañas 
+CREATE TABLE cabanas (
+    id SERIAL PRIMARY KEY,
+    direccion VARCHAR(255) NOT NULL,
+    precio NUMERIC(10, 2) NOT NULL,
+    descripcion TEXT,
+    capacidad INT NOT NULL,
+    reglas TEXT,
+    id_anfitrion INT NOT NULL,
+    id_comision INT, 
+    
+    CONSTRAINT fk_cabana_anfitrion 
+        FOREIGN KEY (id_anfitrion) REFERENCES usuarios(id) ON DELETE RESTRICT
+);
+
+-- Añadí Comisiones 
+CREATE TABLE comisiones (
+    id SERIAL PRIMARY KEY,
+    id_cabana INT NOT NULL,
+    id_usuario INT NOT NULL,
+    porcentaje_comision NUMERIC(5, 2) NOT NULL,
+    fecha_de_cambio TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_comision_cabana 
+        FOREIGN KEY (id_cabana) REFERENCES cabanas(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_comision_usuario 
+        FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE RESTRICT    
+);
+
+-- Relación Cabañas con Comisiones 
+ALTER TABLE cabanas 
+ADD CONSTRAINT fk_cabana_comision 
+    FOREIGN KEY (id_comision) REFERENCES comisiones(id) ON DELETE SET NULL;
+    
+-- Añadí la tabla valoraciones     
+CREATE TABLE valoraciones (
+    id SERIAL PRIMARY KEY,
+    puntuacion INT NOT NULL CHECK (puntuacion >= 1 AND puntuacion <= 5),
+    id_usuario INT NOT NULL,
+    id_cabana INT,
+    id_usuario_valorado INT,
+    
+    CONSTRAINT fk_valoracion_usuario 
+        FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_valoracion_cabana 
+        FOREIGN KEY (id_cabana) REFERENCES cabanas(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_valoracion_usuario_valorado 
+        FOREIGN KEY (id_usuario_valorado) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+-- Añadi tabla imagenes
+CREATE TABLE imagenes (
+    id SERIAL PRIMARY KEY,
+    ruta VARCHAR(255) NOT NULL,
+    id_usuario INT,
+    id_cabana INT,
+    
+    CONSTRAINT fk_imagen_usuario 
+        FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_imagen_cabana 
+        FOREIGN KEY (id_cabana) REFERENCES cabanas(id) ON DELETE CASCADE
+);
+
+CREATE TABLE reservaciones (
+    id SERIAL PRIMARY KEY,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE NOT NULL,
+    id_cabana INT NOT NULL, 
+    id_usuario INT NOT NULL,
+    disponible BOOLEAN DEFAULT TRUE,
+
+    CONSTRAINT fk_reservacion_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE RESTRICT,
+    
+-- Añadí esta fk
+CONSTRAINT fk_reservacion_cabana 
+        FOREIGN KEY (id_cabana) REFERENCES cabanas(id) ON DELETE RESTRICT,
+    
+    CONSTRAINT chk_fechas CHECK (fecha_fin >= fecha_inicio)
+);
+
+CREATE TABLE pagos (
+    id SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    cantidad NUMERIC(10, 2) NOT NULL,
+    tipo_pago VARCHAR(50) NOT NULL,
+    fecha TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    id_reservacion INT NOT NULL,
+
+    CONSTRAINT fk_pago_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE RESTRICT,
+
+    CONSTRAINT fk_pago_reservacion FOREIGN KEY (id_reservacion) REFERENCES reservaciones(id) ON DELETE RESTRICT
+);
+
+CREATE TABLE comentario (
+    id SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,          
+    id_usuario_comentado INT NOT NULL,  
+    id_cabana INT NOT NULL, -- Modifique la ñ por n            
+    comentario TEXT NOT NULL,          
+    fecha_creacion TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, 
+
+    CONSTRAINT fk_comentario_usuario 
+        FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_comentario_usuario_comentado 
+        FOREIGN KEY (id_usuario_comentado) REFERENCES usuarios(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_comentario_cabana 
+        FOREIGN KEY (id_cabana) REFERENCES cabanas(id) ON DELETE CASCADE -- Cambie la ñ por n
+);
+
